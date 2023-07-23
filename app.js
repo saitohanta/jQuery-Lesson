d0e842cd7a08acac4b56f7320eed5859e8c83680
saitohanta <osyogatu17@icloud.com>
2023-07-23 01:35:09 +0000

    [Add]

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ $(function(){
     $('ul').children().css('color', 'red');
   });
 });
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+})
